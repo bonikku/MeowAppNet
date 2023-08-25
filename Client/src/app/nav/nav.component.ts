086be2473135514d1a0719e5265831b7fe89bd27
Nikola <woleet@outlook.com>
2023-08-25 22:33:56 +0200

    Angular routing

--- Client/src/app/nav/nav.component.ts
+++ Client/src/app/nav/nav.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core'
 import { AccountService } from '../_services/account.service'
 import { Observable, of } from 'rxjs'
 import { User } from '../_models/user'
+import { Router } from '@angular/router'
+import { ToastrService } from 'ngx-toastr'
 
 @Component({
   selector: 'app-nav',
@@ -11,18 +13,24 @@ import { User } from '../_models/user'
 export class NavComponent {
   model: any = {}
 
-  constructor(public accountService: AccountService) {}
+  constructor(
+    public accountService: AccountService,
+    private router: Router,
+    private toastr: ToastrService
+  ) {}
 
   login() {
     this.accountService.login(this.model).subscribe({
-      next: (res) => {
-        console.log(res)
+      next: () => this.router.navigateByUrl('/members'),
+      error: (error) => {
+        this.toastr.error(error.error)
+        console.log(error)
       },
-      error: (err) => console.log(err),
     })
   }
 
   logout() {
     this.accountService.logout()
+    this.router.navigateByUrl('/')
   }
 }
